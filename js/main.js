b5abfa8517723e5940cb3c1bb34e0e51917cf9c6
Branko Daskijević <branko.dask@gmail.com>
2023-11-01 17:31:53 +0100

    set first item to have visible play button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const sidebar = document.querySelector(".sidebar.left-section-container")
 const resizer = document.querySelector(".resizer")
 const mainContainerHeaderItems = document.querySelectorAll(".container-item")
+const headerItemsButton = document.querySelectorAll(".container-item-button button")
 
 
 resizer.addEventListener("mousedown", (event) => {
@@ -16,10 +17,26 @@ function resize(event) {
   sidebar.style.flexBasis = dynamicSize
 }
 
+// showing button on mouseover for main container header items
+
 document.addEventListener("DOMContentLoaded", () => {
-  mainContainerHeaderItems.forEach(item => {
+  mainContainerHeaderItems.forEach((item, index) => {
     item.addEventListener("mouseover", () => {
-      console.log("hover")
+      if (index != 0) {
+        headerItemsButton[index].style.visibility = "visible"
+        headerItemsButton[index].style.transitions = "0.5s"
+      }
+    })
+
+    item.addEventListener("mouseout", () => {
+      if (index != 0) {
+        headerItemsButton[index].style.visibility = "hidden"
+        headerItemsButton[index].style.transitions = "0.5s"
+      }
     })
   })
 })
+
+// setting first button to visible playing button
+headerItemsButton[0].style.visibility = "visible"
+headerItemsButton[0].innerHTML = '<i class="fas fa-pause"></i>'
