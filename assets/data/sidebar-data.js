8b4a2df26acdfb6c81164d35e361b8cf9b38be8b
Branko Daskijević <branko.dask@gmail.com>
2023-11-12 12:09:50 +0100

    changed to js for easier import

---
+++ assets/data/sidebar-data.js
@@ -0,0 +1,140 @@
+export const data = [
+  {
+    "cover_src": "/assets/sidebar-album-images/the-social-network.jpg",
+    "cover_alt": "the-social-network",
+    "playlist_title": "The Social Network",
+    "playlist_artist": "Album &#x2022; Trent Reznor and Atticus Ross"
+  },
+  {
+    "cover_src": "/assets/sidebar-album-images/the-dark-knight.jpeg",
+    "cover_alt": "the-dark-knight",
+    "playlist_title": "The Dark Knight",
+    "playlist_artist": "Album &#x2022; Hans Zimmer"
+  },
+  {
+    "cover_src": "/assets/sidebar-album-images/succesion.jpeg",
+    "cover_alt": "succession",
+    "playlist_title": "Succession: Season 1",
+    "playlist_artist": "Album &#x2022; Nicholas Briteli"
+  },
+  {
+    "cover_src": "/assets/sidebar-artist-images/hans-zimmer.jpeg",
+    "cover_alt": "hans-zimmer",
+    "playlist_title": "Hans Zimmer",
+    "playlist_artist": "Artist"
+  },
+  {
+    "cover_src": "/assets/sidebar-album-images/dune.jpeg",
+    "cover_alt": "dune",
+    "playlist_title": "Dune",
+    "playlist_artist": "Album &#x2022; Hand Zimmer"
+  },
+  {
+    "cover_src": "/assets/sidebar-album-images/joker.jpeg",
+    "cover_alt": "joker",
+    "playlist_title": "Joker",
+    "playlist_artist": "Album &#x2022; Hildur Guðnadóttir"
+  },
+  {
+    "cover_src": "/assets/sidebar-artist-images/hildur-guonadottir.jpeg",
+    "cover_alt": "hildur-guonadottir",
+    "playlist_title": "Hildur Guðnadóttir",
+    "playlist_artist": "Artist"
+  },
+  {
+    "cover_src": "/assets/sidebar-album-images/spider-man.jpeg",
+    "cover_alt": "spider-man",
+    "playlist_title": "Spider-Man: Accross the Spider-Verse",
+    "playlist_artist": "Album &#x2022; Daniel Pemberton"
+  },
+  {
+    "cover_src": "/assets/sidebar-artist-images/daniel-pemberton.jpeg",
+    "cover_alt": "daniel-pemberton",
+    "playlist_title": "Daniel Pemberton",
+    "playlist_artist": "Artist"
+  },
+  {
+    "cover_src": "/assets/sidebar-album-images/infinity-war.jpeg",
+    "cover_alt": "infinity-war",
+    "playlist_title": "Avengers: Infinity War",
+    "playlist_artist": "Album &#x2022; Alan Silvestri"
+  },
+  {
+    "cover_src": "/assets/sidebar-album-images/soul.jpeg",
+    "cover_alt": "Soul",
+    "playlist_title": "Soul",
+    "playlist_artist": "Album &#x2022; Various Artists"
+  },
+  {
+    "cover_src": "/assets/sidebar-album-images/oppenheimer.png",
+    "cover_alt": "oppenheimer",
+    "playlist_title": "Oppenheimer",
+    "playlist_artist": "Album &#x2022; Ludwig Görranson"
+  },
+  {
+    "cover_src": "/assets/sidebar-artist-images/ludwig-goransson.jpeg",
+    "cover_alt": "ludwig-goransson",
+    "playlist_title": "Ludwig Goransson",
+    "playlist_artist": "Artist"
+  },
+  {
+    "cover_src": "/assets/sidebar-album-images/inception.jpeg",
+    "cover_alt": "inception",
+    "playlist_title": "Inception",
+    "playlist_artist": "Album &#x2022; Hand Zimmer"
+  },
+  {
+    "cover_src": "/assets/sidebar-album-images/star-wars.webp",
+    "cover_alt": "star-wars",
+    "playlist_title": "Star Wars: A New Hope",
+    "playlist_artist": "Album &#x2022; John Williams"
+  },
+  {
+    "cover_src": "/assets/sidebar-artist-images/john-williams.jpeg",
+    "cover_alt": "john-williams",
+    "playlist_title": "John Williams",
+    "playlist_artist": "Artist"
+  },
+  {
+    "cover_src": "/assets/sidebar-album-images/interstellar.jpeg",
+    "cover_alt": "interstellar",
+    "playlist_title": "Interstellar",
+    "playlist_artist": "Album &#x2022; Hand Zimmer"
+  },
+  {
+    "cover_src": "/assets/sidebar-album-images/mission-impossible.jpeg",
+    "cover_alt": "/mission-impossible",
+    "playlist_title": "Mission: Impossible - Dead Reckoning Part 1",
+    "playlist_artist": "Album &#x2022; Lorne Balfe"
+  },
+  {
+    "cover_src": "/assets/sidebar-album-images/tenet.jpeg",
+    "cover_alt": "tenet",
+    "playlist_title": "Tenet",
+    "playlist_artist": "Album &#x2022; Ludwig Görranson"
+  },
+  {
+    "cover_src": "/assets/sidebar-album-images/the-batman.png",
+    "cover_alt": "the-batman",
+    "playlist_title": "The Batman",
+    "playlist_artist": "Album &#x2022; Michael Giacchino"
+  },
+  {
+    "cover_src": "/assets/sidebar-artist-images/max-richter.jpeg",
+    "cover_alt": "max-richter",
+    "playlist_title": "TMax Richter",
+    "playlist_artist": "Artist"
+  },
+  {
+    "cover_src": "/assets/sidebar-album-images/westworld.jpeg",
+    "cover_alt": "westworld",
+    "playlist_title": "Westworld: Season 3",
+    "playlist_artist": "Album &#x2022; Ramin Djawadi"
+  },
+  {
+    "cover_src": "/assets/sidebar-album-images/westworl1.webp",
+    "cover_alt": "westworl1",
+    "playlist_title": "Westworld: Season 1",
+    "playlist_artist": "Album &#x2022; Ramin Djawadi"
+  }
+]
